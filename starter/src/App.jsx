55c0f57f183a81956609e7af01848749f75d09b7
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-04-03 12:15:14 -0700

    Tours - Fetch and Render tours

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-
+import Loading from "./Loading";
+import Tours from "./Tours";
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
@@ -11,18 +12,29 @@ const fetchTours = async () => {
   try {
 const response = await fetch(url);
 const tours = await response.json()
-console.log(tours);
+setTours(tours);
+
 }catch (error) {
   console.log(error);
 
   }
-}
+  setIsLoading(false)
+};
 
 useEffect(()=> {
 fetchTours();
 },[
 
-])
+]);
+
+if (isLoading){
+  return <main>
+    <Loading />
+  </main>
+};
+return <main>
+  <Tours tours={tours} />
+</main>
 
   return <h2>Tours Starter</h2>;
 };
--- starter/src/Tours.jsx
+++ starter/src/Tours.jsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = ({ tours, removeTour }) => {
+import Tour from "./Tour"
+function Tours({tours}) {
+
   return (
     <section>
-      <div className='title'>
-        <h2>our tours</h2>
-        <div className='title-underline'></div>
-      </div>
-      <div className='tours'>
-        {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
-        })}
-      </div>
+        <div className="title">
+            <h2>our tours</h2>
+            <div className="title-underline"></div>
+        </div>
+        <div className="tours">{tours.map((tour) => {
+            // console.log(tour);
+         return   <Tour key={tour.id} {...tour} />
+        })}</div>
     </section>
-  );
-};
-
-export default Tours;
+  )
+}
+export default Tours
